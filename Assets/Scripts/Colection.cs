f89579a319f584c171903d1e13598c4e694895fd
urzurioliver <urzurioliver@gmail.com>
2026-06-01 14:32:37 -0300

    Initial commit

---
+++ Assets/Scripts/Colection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Colection : MonoBehaviour
+{
+     public TextMeshProUGUI txt_contador;
+     private int objetosRecolectados = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt_contador.text = ("objetos recolectados: " + objetosRecolectados);
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Coleccionable"))
+        {
+        Destroy(col.gameObject);
+        Debug.Log("objeto recolectado");
+        objetosRecolectados++;
+        }
+}
+}
