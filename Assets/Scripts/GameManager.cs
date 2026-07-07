0fb696bb61a6d870d409a73519458ba0f57fd717
urzurioliver <urzurioliver@gmail.com>
2026-07-07 08:45:19 -0300

    arreglé un detalle, ya terminado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 
 {
-    
-    public AudioSource loseSound;
+
     float timer = 60;
     public UIMANAGER uiManagerScript;
 
@@ -26,7 +25,7 @@ public class GameManager : MonoBehaviour
             uiManagerScript.UpdateTimer(timer);
             Time.timeScale = 0;
             uiManagerScript.MostrarPantallaGameOver();
-            loseSound.Play();
+            
         }
         else {
             uiManagerScript.UpdateTimer(timer);
